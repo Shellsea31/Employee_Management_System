e00aafdcb853cc4fe48875d1ec96f23a6d17da4e
Shelsy Elaine <elainesbarrera@gmail.com>
2020-12-26 19:52:17 -0800

    started up modules, made connection to database

--- app.js
+++ app.js
@@ -1,2 +1,24 @@
-const inquirer = require ("inquirer");
-const mysql = require ("mysql");
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "ShellseaFS2020!",
+  database: "management_db",
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log(`connected as id: ${connection.threadId}`);
+  findAllDepart();
+  connection.end();
+});
+
+const findAllDepart = () => {
+  connection.query("SELECT * FROM department", (err, res) => {
+    if (err) throw err;
+    console.log(res);
+  });
+};
